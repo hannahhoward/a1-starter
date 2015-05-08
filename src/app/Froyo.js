c3c02e2909e145da1e1793a43cd7362e0306df57
Hannah Howard <hannah@hannahhoward.net>
2015-05-07 18:32:54 -0700

    Commit

--- src/app/Froyo.js
+++ src/app/Froyo.js
@@ -4,20 +4,49 @@ import {Service} from "a1atscript";
 export default class Froyo {
   froyos = [
     {
+      id: 1,
       flavor: "Chocolate",
       cost: "$2.00"
     },
     {
+      id: 2,
       flavor: "Vanilla",
-      cost: "$2.00"
+      cost: "$1.00"
     }
   ];
+  idCounter = 3;
+
+  buildNew() {
+    return {
+      id: null,
+      flavor: "",
+      cost: ""
+    }
+  }
+
+  save(froyo) {
+    var index = this.froyos.indexOf(testFroyo => testFroyo.id == froyo.id)
+    if (index == -1) {
+      froyo.id = this.idCounter;
+      this.idCounter += 1;
+      this.froyos.push(froyo);
+    } else {
+      this.froyos[index] = froyo;
+    }
+  }
+
+  destroy(froyo) {
+    var index = this.froyos.indexOf(testFroyo => testFroyo.id == froyo.id)
+    if (index != -1) {
+      this.froyos.splice(index, 1);
+    }
+  }
 
   all() {
     return this.froyos;
   }
 
   find(n) {
-    return this.froyos[n];
+    return this.froyos.find(froyo => froyo.id == n);
   }
 }
